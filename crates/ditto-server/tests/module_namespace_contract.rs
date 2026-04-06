636d937424930b27f6cd3856d4f9d57e866aa77a
shiertier <junjie.text@gmail.com>
2026-04-06 20:05:49 +0800

    test(gateway): align facade contract with public surface

--- crates/ditto-server/tests/module_namespace_contract.rs
+++ crates/ditto-server/tests/module_namespace_contract.rs
@@ -3,6 +3,7 @@ use std::any::TypeId;
 #[test]
 fn direct_l0_namespaces_expose_public_facades() {
     let _ = TypeId::of::<ditto_core::error::DittoError>();
+    let _ = TypeId::of::<dyn ditto_core::llm_core::model::LanguageModel>();
     let _ = TypeId::of::<ditto_core::contracts::RuntimeRoute>();
     let _ = TypeId::of::<ditto_core::contracts::FinishReason>();
     let _ = TypeId::of::<ditto_core::contracts::Usage>();
@@ -10,10 +11,8 @@ fn direct_l0_namespaces_expose_public_facades() {
     let _ = TypeId::of::<ditto_core::contracts::GenerateRequest>();
     let _ = TypeId::of::<ditto_core::contracts::GenerateResponse>();
     let _ = TypeId::of::<ditto_core::contracts::StreamChunk>();
+    let _ = TypeId::of::<ditto_core::config::ProviderConfig>();
     let _ = TypeId::of::<ditto_core::provider_options::ProviderOptionsEnvelope>();
-    let _ = TypeId::of::<ditto_core::runtime_registry::RuntimeRegistrySnapshot>();
-    let _ = ditto_core::runtime::build_language_model;
-    let _ = ditto_core::runtime::build_context_cache_model;
 }
 
 #[test]
@@ -26,8 +25,10 @@ fn northbound_capability_facades_remain_available() {
 #[cfg(feature = "gateway")]
 #[test]
 fn gateway_root_exports_stable_public_facades() {
-    let _ = TypeId::of::<ditto_server::gateway::GatewayHttpState>();
+    let _ = TypeId::of::<ditto_server::gateway::Gateway>();
+    let _ = TypeId::of::<ditto_server::gateway::GatewayConfig>();
+    let _ = TypeId::of::<ditto_server::gateway::BackendConfig>();
+    let _ = TypeId::of::<ditto_server::gateway::VirtualKeyConfig>();
+    let _ = TypeId::of::<ditto_server::gateway::RouteBackend>();
     let _ = TypeId::of::<ditto_server::gateway::GatewayRequest>();
-    let _ = TypeId::of::<ditto_server::gateway::ProxyBackend>();
-    let _ = TypeId::of::<ditto_server::gateway::GatewayStateFile>();
 }
